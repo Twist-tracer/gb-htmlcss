f8cff66831c4dd5a3db7f03acdd2a9ae32470ca5
twist-tracer <twist.tracer@gmail.com>
2020-11-12 23:02:05 +0300

    <master> gulp

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# [GB] html&css trainings
+# [GB] html&css lessons
 
+## Структура проекта
+
+    assets/             Собранный проект
+    node_modules/       Библиотеки
+    src/                Исходники
+
+## Сборка проекта
+
+**1. Качаем [node и npm](https://nodejs.org/en/)**
+
+**2. Ставим [yarn](https://yarnpkg.com/)**
+
+```shell script
+npm install -g yarn
+```
+
+**3. Подтягиваем зависимости**
+
+```shell script
+yarn install
+```
+
+**4. Собираем проект**
+
+```shell script
+yarn build
+```
---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const {src, dest, series, watch} = require('gulp');
+const gulpClean = require('gulp-clean');
+const gulpSass = require('gulp-sass');
+const gulpCssMin = require('gulp-cssmin');
+const gulpRename = require('gulp-rename');
+
+function clean() {
+    return src('./assets/*')
+        .pipe(gulpClean());
+}
+
+function copy(cb) {
+    src(['./src/*.html'])
+        .pipe(dest('./assets'));
+
+    src(['./src/img/*'])
+        .pipe(dest('./assets/img'));
+
+    return cb();
+}
+
+function sass() {
+    return src('./src/css/**/*.scss')
+        .pipe(gulpSass({outputStyle: 'expanded'}))
+        .pipe(dest('./assets/css'));
+}
+
+function cssMin() {
+    return src('./assets/css/**/*.css')
+        .pipe(gulpCssMin())
+        .pipe(gulpRename({suffix: '.min'}))
+        .pipe(dest('./assets/css'))
+}
+
+function build() {
+    return series(clean, copy, sass, cssMin);
+}
+
+exports.watch = function() {
+    return watch('./src/**/*', build());
+};
+
+exports.default = exports.build = build();
